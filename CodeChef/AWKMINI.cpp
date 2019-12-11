eee843448dc8a9938a357a37f81fd5073a570386
destinyson7 <tanish.lad@research.iiit.ac.in>
2019-12-11 22:48:59 +0530

    Solved CC MAXDIVER and AWKMINI

---
+++ CodeChef/AWKMINI.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair <ll, ll> pll;
+typedef pair <int, int> pii;
+
+typedef tree <ll, null_type, less <ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// order_of_key(val): returns the number of values less than val
+// find_by_order(k): returns an iterator to the kth largest element (0-based)
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(a) a.begin(), a.end()
+#define sz(a) (ll)(a.size())
+#define endl "\n"
+
+template <class Ch, class Tr, class Container>
+basic_ostream <Ch, Tr> & operator << (basic_ostream <Ch, Tr> & os, Container const& x) 
+{
+    os << "{ ";
+    for(auto& y : x) 
+    {
+        os << y << " ";
+    }
+    return os << "}";
+}
+
+template <class X, class Y>
+ostream & operator << (ostream & os, pair <X, Y> const& p) 
+{
+    return os << "[" << p.ff << ", " << p.ss << "]";
+}
+
+ll gcd(ll a, ll b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+
+    return gcd(b, a%b);
+}
+
+ll modexp(ll a, ll b, ll c)
+{   
+    a%=c;
+
+    ll ans = 1;
+
+    while(b)
+    {
+        if(b&1)
+        {
+            ans = (ans*a)%c;
+        }
+
+        a = (a*a)%c;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+const ll L = 1e5+5;
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    ll n = sz(s);
+
+    ll cntb = 0, cntg = 0;
+
+    for(ll i=0; i<n; i++)
+    {
+        if(s[i] == 'b')
+        {
+            cntb++;
+        }
+
+        else
+        {
+            cntg++;
+        }
+    }
+
+    ll ff = max(cntb, cntg);
+    ll ss = min(cntb, cntg);
+
+    ll rem = ff - ss;
+    ll left = rem/2;
+    ll right = rem - left;
+
+    string final = "";
+
+    for(ll i=0; i<left; i++)
+    {
+        final += "b";
+    }
+
+    for(ll i=0; i<ss; i++)
+    {
+        final += "bg";
+    }
+
+    for(ll i=0; i<right; i++)
+    {
+        final += "b";
+    }
+
+    ll ans = 0;
+
+    ll curb = 0, curg = 0;
+    ll sumb = 0, sumg = 0;
+
+    for(ll i=0; i<n; i++)
+    {
+        if(final[i] == 'b')
+        {
+            if(curg > 0)
+            {
+                ans += i*curg;
+                ans -= sumg;
+            }
+
+            curb++;
+            sumb += i;
+        }
+
+        else
+        {
+            if(curb > 0)
+            {
+                ans += i*curb;
+                ans -= sumb;
+            }
+
+            curg++;
+            sumg += i;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    ll T;
+    cin >> T;
+
+    while(T--)
+    {
+        solve();
+    }
+
+    return 0;
+}
