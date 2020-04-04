3236b5abcb7915ae96a18bdbd8184025cb83de34
destinyson7 <tanish.lad@research.iiit.ac.in>
2020-04-04 15:39:55 +0530

    added code jam 20 qualification round codes

---
+++ Google/CodeJam2020/QualificationRound/ParentingPartneringReturns.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair <ll, ll> pll;
+typedef pair <int, int> pii;
+
+typedef tree <ll, null_type, less <ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// order_of_key(val): returns the number of values less than val
+// find_by_order(k): returns an iterator to the kth largest element (0-based)
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(a) a.begin(), a.end()
+#define sz(a) (ll)(a.size())
+#define endl "\n"
+
+template <class Ch, class Tr, class Container>
+basic_ostream <Ch, Tr> & operator << (basic_ostream <Ch, Tr> & os, Container const& x) 
+{
+    os << "{ ";
+    for(auto& y : x) 
+    {
+        os << y << " ";
+    }
+    return os << "}";
+}
+
+template <class X, class Y>
+ostream & operator << (ostream & os, pair <X, Y> const& p) 
+{
+    return os << "[" << p.ff << ", " << p.ss << "]";
+}
+
+ll gcd(ll a, ll b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+
+    return gcd(b, a%b);
+}
+
+ll modexp(ll a, ll b, ll c)
+{   
+    a%=c;
+
+    ll ans = 1;
+
+    while(b)
+    {
+        if(b&1)
+        {
+            ans = (ans*a)%c;
+        }
+
+        a = (a*a)%c;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+ll nxt()
+{
+    ll x;
+    cin >> x;
+    return x;
+}
+
+const ll L = 1e5+5;
+
+bool cmp(pair<pll, ll> a, pair<pll, ll> b)
+{
+    if(a.ff.ss == b.ff.ss)
+    {
+        return a.ff.ff > b.ff.ff;
+    }
+
+    return a.ff.ss < b.ff.ss;
+}
+
+void solve(ll tc)
+{
+    ll n = nxt();
+
+    vector <pair<pll, ll>> p;
+
+    vector <char> ans(n, '0');
+
+    for(ll i=0; i<n; i++)
+    {
+        ll s = nxt(), e = nxt();
+        p.pb(mp(mp(s, e), i));
+    }
+
+    sort(all(p), cmp);
+
+    ll ce = -1, je = -1;
+
+    for(ll i=0; i<n; i++)
+    {
+        if(ce <= p[i].ff.ff)
+        {
+            ans[p[i].ss] = 'C';
+            ce = p[i].ff.ss;
+        }
+
+        else if(je <= p[i].ff.ff)
+        {
+            ans[p[i].ss] = 'J';
+            je = p[i].ff.ss;
+        }
+
+        else
+        {
+            cout << "Case #" << tc << ": " << "IMPOSSIBLE" << endl;
+            return;
+        }
+    }
+
+    cout << "Case #" << tc << ": ";
+    for(auto i: ans)
+    {
+        cout << i;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    ll T = 1;
+    T = nxt();
+
+    for(ll i=1; i<=T; i++)
+    {
+        solve(i);
+    }
+
+    return 0;
+}
