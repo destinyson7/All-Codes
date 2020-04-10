df0c473ef3deac625316a41dd6feaa65444a3f1e
destinyson7 <tanish.lad@research.iiit.ac.in>
2020-04-10 22:13:23 +0530

    added codes of cf edu round 85

---
+++ Codeforces/1334/D.cpp
@@ -0,0 +1,205 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair <ll, ll> pll;
+typedef pair <int, int> pii;
+
+typedef tree <ll, null_type, less <ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// order_of_key(val): returns the number of values less than val
+// find_by_order(k): returns an iterator to the kth largest element (0-based)
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(a) a.begin(), a.end()
+#define sz(a) (ll)(a.size())
+#define endl "\n"
+
+template <class Ch, class Tr, class Container>
+basic_ostream <Ch, Tr> & operator << (basic_ostream <Ch, Tr> & os, Container const& x) 
+{
+    os << "{ ";
+    for(auto& y : x) 
+    {
+        os << y << " ";
+    }
+    return os << "}";
+}
+
+template <class X, class Y>
+ostream & operator << (ostream & os, pair <X, Y> const& p) 
+{
+    return os << "[" << p.ff << ", " << p.ss << "]";
+}
+
+ll gcd(ll a, ll b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+
+    return gcd(b, a%b);
+}
+
+ll modexp(ll a, ll b, ll c)
+{   
+    a%=c;
+
+    ll ans = 1;
+
+    while(b)
+    {
+        if(b&1)
+        {
+            ans = (ans*a)%c;
+        }
+
+        a = (a*a)%c;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+ll nxt()
+{
+    ll x;
+    cin >> x;
+    return x;
+}
+
+const ll L = 1e5+5;
+
+void solve()
+{
+    ll n = nxt(), l = nxt(), r = nxt();
+
+    ll cur = 0;
+
+    pll start_l, start_r;
+
+    basic_string <ll> ans;
+
+    for(ll i=1; i<=n; i++)
+    {
+        if(cur + 2*(n-i) >= l) 
+        {
+            ll ahead = l - cur;
+
+            if(ahead & 1)
+            {
+                start_l.ff = i;
+                ans += i;
+
+                ans += (i + 1 + ahead/2);
+
+                for(ll j=(i+1+ahead/2)+1; j<=n; j++) {
+                    ans += i;
+                    ans += j;
+                }
+
+                ll cnt = sz(ans);
+
+                for(ll j=i+1; j<=n; j++) {
+                    for(ll k=j+1; k<=n; k++) {
+                        if(cnt >= r-l+1) {
+                            // cout << ans << endl;
+                            for(ll put = 0; put < r-l+1; put++) {
+                                cout << ans[put] << " ";
+                            }
+                            cout << endl;
+                            return;
+                        }
+
+                        ans += j;
+                        ans += k;
+
+                        cnt += 2;
+                    }
+                }
+
+                if(cnt < r-l+1) {
+                    ans += 1;
+                    cnt++;
+                }
+
+                for(ll put = 0; put < r-l+1; put++) {
+                    cout << ans[put] << " ";
+                }
+                cout << endl;
+                return;
+            }
+
+            else
+            {
+                start_l.ff = i + ahead/2;
+                ans += (i + ahead/2);
+
+                for(ll j=(i+ahead/2)+1; j<=n; j++) {
+                    ans += i;
+                    ans += j;
+                }
+
+                ll cnt = sz(ans);
+
+                for(ll j=i+1; j<=n; j++) {
+                    for(ll k=j+1; k<=n; k++) {
+                        if(cnt >= r-l+1) {
+                            for(ll put = 0; put < r-l+1; put++) {
+                                cout << ans[put] << " ";
+                            }
+                            cout << endl;
+                            return;
+                        }
+
+                        ans += j;
+                        ans += k;
+
+                        cnt += 2;
+                    }
+                }
+
+                if(cnt < r-l+1) {
+                    ans += 1;
+                    cnt++;
+                }
+
+                for(ll put = 0; put < r-l+1; put++) {
+                    cout << ans[put] << " ";
+                }
+                cout << endl;
+                return;
+            }
+
+        }
+
+        cur += 2*(n-i);
+    }
+
+    cout << 1 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    ll T = 1;
+    T = nxt();
+
+    while(T--)
+    {
+        solve();
+    }
+
+    return 0;
+}
