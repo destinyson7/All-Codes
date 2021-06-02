53a7af8b7bb5d2ef75d733a3b90ddf3b6040f5cb
destinyson7 <tanish.lad@research.iiit.ac.in>
2021-06-02 20:00:15 +0530

    discrete math binary search cf problem

---
+++ Codeforces/cf/contest/555/d/d.cpp
@@ -0,0 +1,177 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair <ll, ll> pll;
+typedef pair <int, int> pii;
+
+typedef tree <ll, null_type, less <ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// order_of_key(val): returns the number of values less than val
+// find_by_order(k): returns an iterator to the kth largest element (0-based)
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(a) a.begin(), a.end()
+#define sz(a) (ll)(a.size())
+#define endl "\n"
+
+template <class Ch, class Tr, class Container>
+basic_ostream <Ch, Tr> & operator << (basic_ostream <Ch, Tr> & os, Container const& x)
+{
+    os << "{ ";
+    for(auto& y : x)
+    {
+        os << y << " ";
+    }
+    return os << "}";
+}
+
+template <class X, class Y>
+ostream & operator << (ostream & os, pair <X, Y> const& p)
+{
+    return os << "[" << p.ff << ", " << p.ss << "]";
+}
+
+ll gcd(ll a, ll b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+
+    return gcd(b, a%b);
+}
+
+ll modexp(ll a, ll b, ll c)
+{
+    a%=c;
+
+    ll ans = 1;
+
+    while(b)
+    {
+        if(b&1)
+        {
+            ans = (ans*a)%c;
+        }
+
+        a = (a*a)%c;
+        b >>= 1;
+    }
+
+    return ans;
+}
+
+ll nxt()
+{
+    ll x;
+    cin >> x;
+    return x;
+}
+
+const ll L = 2e5+5;
+ll n, m;
+ll x[L];
+ll old_x[L];
+map <ll, ll> ind;
+map <ll, ll> new_ind;
+
+ll res(ll cur, ll len, bool dir = true)
+{
+    if(dir)
+    {
+        ll right = upper_bound(x, x + n, x[cur] + len) - x - 1;
+        len -= (x[right] - x[cur]);
+
+        ll left = lower_bound(x, x + n, x[right] - len) - x;
+        len -= (x[right] - x[left]);
+
+        if(cur == right && cur == left)
+        {
+            // cout << cur << " " << left << " " << right << endl;
+            return cur;
+        }
+
+        if(left != right)
+        {
+            len %= (2 * (x[right] - x[left]));
+        }
+
+        // cout << cur << " " << len << " " << left << " " << right << " " << ll(dir) << endl;
+
+        return res(left, len, dir);
+    }
+
+    // else
+    // {
+    //     ll left = lower_bound(x, x + n, x[cur] - len) - x;
+    //     len -= (x[cur] - x[left]);
+
+    //     ll right = upper_bound(x, x + n, x[left] + len) - x - 1;
+    //     len -= (x[right] - x[left]);
+
+    //     if(cur == right && cur == left)
+    //     {
+    //         return cur;
+    //     }
+
+    //     if(left != right)
+    //     {
+    //         len %= (2 * (x[right] - x[left]));
+    //     }
+
+    //     cout << cur << " " << len << " " << left << " " << right << " " << ll(dir) << endl;
+
+    //     return res(left, len, dir ^ 1);
+    // }
+}
+
+void solve()
+{
+    n = nxt(), m = nxt();
+
+    for(ll i = 0; i < n; i++)
+    {
+        cin >> x[i];
+        ind[x[i]] = i;
+        old_x[i] = x[i];
+    }
+
+    sort(x, x + n);
+
+    for(ll i = 0; i < n; i++)
+    {
+        new_ind[x[i]] = i;
+    }
+
+    while(m--)
+    {
+        ll a = nxt(), l = nxt();
+
+        cout << ind[x[res(new_ind[old_x[a - 1]], l, true)]] + 1 << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    ll T = 1;
+    // T = nxt();
+
+    while(T--)
+    {
+        solve();
+    }
+
+    return 0;
+}
